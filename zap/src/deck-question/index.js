469df0efa3ebb130ab55afa8990c2d4a23757e5d
MariaDuda <mariabevenuto062@gmail.com>
2022-05-20 13:14:23 -0300

    deck questios with map

--- zap/src/deck-question/index.js
+++ zap/src/deck-question/index.js
@@ -1,5 +1,28 @@
-export default function DeckQuestion(){
-    return(
-        <h1>oi</h1>
+import data from "../data"
+import "./style.css"
+const logoMenor = "assets/imgs/logo-pequeno.png"
+const setinha = "assets/imgs/setinha.png"
+export default function DeckQuestion() {
+    return (
+        <>
+        <div className="logoMenor">
+            <img className="pequena" src={logoMenor} alt="" />
+            <h2 className="text">ZapRecall</h2>
+        </div>
+         <main>
+
+         <div className="centralizar fixar">
+             {data.map((perg, index) => {
+                 return (
+                     <div className="fundo-deck">
+                         <h2 className="perguntaObj">{perg.react}</h2>
+                         <img className="setinha"src={setinha} alt="" />
+                     </div>
+                 )
+             })}
+
+         </div>
+     </main>
+ </>
     )
 }
