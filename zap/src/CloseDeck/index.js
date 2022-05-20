9e86a31a5db96e74fd758a2d99936c78978c415f
MariaDuda <mariabevenuto062@gmail.com>
2022-05-20 12:39:47 -0300

    pergunta com index

--- zap/src/CloseDeck/index.js
+++ zap/src/CloseDeck/index.js
@@ -1,5 +1,24 @@
-export default function CloseDeck(){
-    return(
-        <h1>oi</h1>
+import data from "../data"
+import "./style.css"
+const logoMenor = "assets/imgs/logo-pequeno.png"
+
+export default function CloseDeck() {
+    return (
+        <>
+            <div className="logoMenor">
+                <img  className="pequena" src={logoMenor} alt="" />
+                <h2 className="text">ZapRecall</h2>
+            </div>
+
+            <div className="centralizar">
+            {data.map((perg , index) =>{
+                     return(
+                        <div className="fundo-branco"><h2 className="pergunta">pergunta {index}</h2></div>
+                     )
+                 })}
+                
+            </div>
+
+        </>
     )
 }
---
+++ zap/src/data.js
@@ -0,0 +1,42 @@
+const data = [{
+    question: "pergunta 1",
+    react: " o React é_",
+    resp: "Uma biblioteca JavaScript para construção de interfaces"
+},
+{
+    question: "pergunta 2",
+    react: "O que é JSX? ",
+    resp: "Uma extensão de linguagem do JavaScript"
+},
+{
+    question: "pergunta 3",
+    react: "componentes devem inicar com__",
+    resp: "Letra maiúscula"
+},
+{
+    question: "pergunta 4",
+    react: "podemos colocar __ dentro do JSX",
+    resp: "Expressões",
+},
+{
+    question: "pergunta 5",
+    react: " o ReactDom nos ajuda__",
+    resp: "Interagindo com a DOM para colocar componentes React na tela"
+},
+{
+    question: "pergunta 6",
+    react: "usamos npm para__",
+    resp: "Gerenciar os pacotes necessários e suas dependências"
+},
+{
+    question: "pergunta 7",
+    react: "usamos props para__ ",
+    resp: "Passar diferentes informações para componentes"
+},
+{
+    question: "pergunta 8",
+    react: "usamos estad0 (state) para __",
+    resp: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
+}
+]
+export default data;
