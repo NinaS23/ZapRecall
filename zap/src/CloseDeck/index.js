dad1cd764f6b5e09e6c742b4c4e22f4879d5e834
MariaDuda <mariabevenuto062@gmail.com>
2022-05-20 12:46:55 -0300

    2 screen done

--- zap/src/CloseDeck/index.js
+++ zap/src/CloseDeck/index.js
@@ -1,24 +1,28 @@
 import data from "../data"
 import "./style.css"
 const logoMenor = "assets/imgs/logo-pequeno.png"
-
+const seta = "assets/imgs/seta.png"
 export default function CloseDeck() {
     return (
         <>
             <div className="logoMenor">
-                <img  className="pequena" src={logoMenor} alt="" />
+                <img className="pequena" src={logoMenor} alt="" />
                 <h2 className="text">ZapRecall</h2>
             </div>
+         <main>
+           
+                <div className="centralizar">
+                    {data.map((perg, index) => {
+                        return (
+                            <div className="fundo-branco">
+                                <h2 className="pergunta">pergunta {index}</h2>
+                                <img className="seta" src={seta} alt="" />
+                            </div>
+                        )
+                    })}
 
-            <div className="centralizar">
-            {data.map((perg , index) =>{
-                     return(
-                        <div className="fundo-branco"><h2 className="pergunta">pergunta {index}</h2></div>
-                     )
-                 })}
-                
-            </div>
-
+                </div>
+            </main>
         </>
     )
 }
